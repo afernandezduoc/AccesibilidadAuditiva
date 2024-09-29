1f1290cef5bd7b40cf53dd58cc6975c26705759f
Alejandro Fernandez <alej.fernandez@duocuc.cl>
2024-09-29 12:32:16 -0300

    Se implementa la funcion de Escribir para mostrar en pantalla el audio escuchado

--- app/src/main/java/com/example/accesibilidadauditiva/MainActivity.kt
+++ app/src/main/java/com/example/accesibilidadauditiva/MainActivity.kt
@@ -63,6 +63,7 @@ fun MainScreen(navController: NavHostController, isAuthenticated: Boolean, onLog
             composable("recovery") { PasswordRecoveryScreen(navController) }
             composable("profile") { ProfileScreen() }
             composable("settings") { SettingsScreen(navController) }
+            composable("escribir") { EscribirScreen(navController) }
         }
     }
 }
